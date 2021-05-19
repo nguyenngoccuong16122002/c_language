3e0994e206e439ce489212f05e038841d49442b8
Cuong Nguyen <nguyenngoccuong.16122002@hotmail.com>
2021-05-19 16:07:47 +0700

    commit

---
+++ src/baitap.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class baitap{
+  public static void main(String[] args) {
+      int x=32;
+      String s1= String.format("%#x",x);
+      System.out.println(s1);
+  }
+    
+}
